640688dad70b8566489d37e81a667d33b369617c
Patrick Lestrange <plestran@uw.edu>
2017-10-21 09:03:08 -0700

    added bootstrap theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
@@ -20,14 +20,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('GitHub', 'https://github.com/plestran/'),
+         ('UW Website', 'http://depts.washington.edu/ligroup/people/patrick-lestrange/'),
+         ('Google Scholar', 'https://scholar.google.com/citations?user=_p09HUIAAAAJ&hl=en'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -38,3 +37,5 @@ MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
+
+THEME = "/Users/plestran/Dropbox/flash-facts/bootstrap"
